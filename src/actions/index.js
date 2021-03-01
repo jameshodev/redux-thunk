a8e2a63889cb7b6d0037ceaa22cc86dade63582c
James Ho <jamesho.dev@gmail.com>
2021-03-01 15:25:14 -0800

    reudx-thunk

---
+++ src/actions/index.js
@@ -0,0 +1,12 @@
+// Action creator
+import jsonPlaceholder from '../apis/jsonPlaceholder';
+
+// The result of using async/await causes an error
+// "Actions must be plain objects" during the transccopilier converting
+// ECMA Script2015 to older javascript engine. It returns the request
+// instead of the objects from the action creator.
+
+export const fetchPosts = () => async dispatch => {
+  const response = await jsonPlaceholder.get('/posts');
+  dispatch({type: 'FETCH_POSTS', payload: response });
+};
